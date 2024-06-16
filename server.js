21de88e88e211bb2667e84e4528a58746f2afbb9
l0c4lxid <syaidxandhika@gmail.com>
2024-06-16 11:34:45 +0700

    update server.js

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const Hapi = require("@hapi/hapi");
+const path = require("path");
+const fs = require("fs");
 const routes = require("./routes");
 
 const init = async () => {
@@ -9,6 +11,17 @@ const init = async () => {
 
   // Registrasi routes
   server.route(routes);
+
+  // Menambahkan route untuk menangani 404
+  server.ext("onPreResponse", (request, h) => {
+    const response = request.response;
+    if (response.isBoom && response.output.statusCode === 404) {
+      const filePath = path.join(__dirname, "404.html");
+      const fileContent = fs.readFileSync(filePath, "utf8");
+      return h.response(fileContent).code(404).type("text/html");
+    }
+    return h.continue;
+  });
 
   await server.start();
   console.log("Server running on %s", server.info.uri);
